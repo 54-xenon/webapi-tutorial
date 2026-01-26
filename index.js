facbf54b6598cb84288f0d2b23851c547ecd2418
54-xenon <congas.rabbi_0b@icloud.com>
2026-01-26 16:43:06 +0900

    基礎部分完了

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+// exporess(モジュールの読み込み)
 const express = require('express');
+// 変数appに代入している
 const app = express();
+// 使用するポート番号の指定 -> 3000番、8080番がよく使われる
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
+// 指定のポートで開く
 app.listen(port, () => {
     console.log(`繋がったよ:http://localhost:${port}`)
+});
+
+app.get('/',(req, res) => {
+    res.send('Hello World!');
 });
